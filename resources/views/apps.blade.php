d61f382ec724956e797e4ed0efad684eaf9faa75
giduya <alan@edulap.com>
2023-01-31 19:25:57 -0600

    16

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -12,11 +12,7 @@
         </li>
         <li class="xn-profile">
           <a href="#" class="profile-mini">
-            @if(Auth::user()->avatar)
-              <img src="{{ asset($ruta->ruta_img().Auth::user()->avatar) }}" alt="{{ Auth::user()->nombre }} {{ Auth::user()->apellido_paterno }}">
-            @else
-              <img src="{{ asset('app/img/avatar.png') }}" alt="{{ Auth::user()->nombre }} {{ Auth::user()->apellido_paterno }}">
-            @endif
+            <img src="{{ asset('app/img/avatar.png') }}" alt="{{ Auth::user()->nombre }} {{ Auth::user()->apellido_paterno }}">
           </a>
           <div class="profile">
             <div class="profile-image">
