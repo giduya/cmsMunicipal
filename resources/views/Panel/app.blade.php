d68bfa9772b0366e0ea3bf0f166d08806b3855fc
giduya <alan@edulap.com>
2023-02-01 12:53:39 -0600

    33

--- resources/views/Panel/app.blade.php
+++ resources/views/Panel/app.blade.php
@@ -3,7 +3,7 @@
 <body>
   <div id="fb-root"></div>
   <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
-  <div class="page-container @if(Auth::user()->menu == 0) page-navigation-toggled page-container-wide @endif">
+  <div class="page-container">
     <div class="page-sidebar page-sidebar-fixed scroll">
       <ul class="x-navigation x-navigation-custom @if(Auth::user()->menu == 0) x-navigation-minimized @endif">
         <li class="xn-logo">
