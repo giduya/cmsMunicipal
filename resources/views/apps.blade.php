5370459db76133b77b3f6c304b34427ca36affff
giduya <alan@edulap.com>
2023-02-01 10:35:57 -0600

    21

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -1,105 +1,348 @@
-@include('Panel.head')
+@extends('Panel.os')
 
-<body>
-  <div id="fb-root"></div>
-  <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
-  <div class="page-container page-navigation-toggled page-container-wide ">
-    <div class="page-sidebar page-sidebar-fixed scroll">
-      <ul class="x-navigation x-navigation-custom x-navigation-minimized">
-        <li class="xn-logo">
-          <a>Ayuntamiento Digital</a>
-          <a href="#" class="x-navigation-control"></a>
-        </li>
-        <li class="xn-profile">
-          <a href="#" class="profile-mini">
-            <img src="{{ asset('app/img/avatar.png') }}" alt="">
-          </a>
-          <div class="profile">
-            <div class="profile-image">
-              <img src="{{ asset('app/img/avatar.png') }}" alt="">
-            </div>
-            <div class="profile-data">
-              <div class="profile-data-name">Bienvenido/a</div>
-              <div class="profile-data-title"></div>
-            </div>
-            <div class="profile-controls">
-              <a data-toggle="tooltip" data-placement="top" data-original-title="Perfil" class="profile-control-left" href="{{ URL::to('profile') }}"><span class="glyphicon glyphicon-user"></span></a>
-              <a data-toggle="tooltip" data-placement="top" data-original-title="Ayuda"  class="profile-control-right" @yield('help')><span class="fa fa-question"></span></a>
-            </div>
-          </div>
-        </li>
-
-        <li class="">
-          <a href="{{ url('/apps') }}">
-            <span class="far fa-th"></span> &nbsp;
-            <span class="xn-text"> Regresar a las Apps</span>
-          </a>
-        </li>
-
-        @yield('menu')
-      </ul><!-- END X-NAVIGATION -->
-    </div><!-- END PAGE SIDEBAR -->
-
-    <div class="page-content">
-
-      <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
-        <li class="xn-icon-button">
-          <a href="" class="x-navigation-minimize"><span class="far fa-indent far fa-outdent"></span></a>
-        </li><!-- END TOGGLE NAVIGATION -->
-
-        <li class="xn-icon-button pull-right last">
-          <a href="#" class="mb-control" data-box="#mb-signout"><span class="far fa-power-off"></span></a>
-        </li><!-- END POWER OFF -->
-
-      </ul><!-- END X-NAVIGATION VERTICAL -->
+@section('wall')
+style="background-image: url('app/img/backgrounds/bg1.jpg') !important; background-attachment:fixed !important; padding-bottom:3px;"
+@endsection
 
 
-      <ul class="breadcrumb">
-        <li><a href="{{ url('apps') }}">Apps</a></li>
-        @yield('breadcumb')
-      </ul><!-- END BREADCRUMB -->
+@section('content')
+<div class="app_container">
 
 
-      <div class="page-content-wrap">
-
-        @hasSection ('h2')
-        <div class="page-title">
-          @yield('h2')
-        </div>
-        @endif
-
-        <div class="row">
-          <div class="col-md-12" id="alertas">
-            @include('Panel.alert')
-          </div>
-        </div><!--row-->
+  <div class="alert alert-warning" role="alert">
+    <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Cerrar</span></button>
+    ¿Sabías que en mayo del 2020, da comienzo el gobierno electrónico? Ya estamos preparando las capacitaciones y manuales para ayudarte en este cambio. <code>ABRIL 2020</code>
+  </div>
 
 
-        @hasSection('content')
-          @yield('content')
-        @else
+  @if(CARPETA == "startup")
+  <div class="app_icon">
+    <a href="{{ url('rif') }}">
+      <img src="app/img/apps/rif_icon.png" alt="Startup"/>
+      <span class="app_text">
+        Mi Startup <br/><br/>
+      </span>
+    </a>
+  </div>
+  @endif
 
-          @yield('row-widgets')
 
-          <div class="row">
-            <div class="col-md-12">
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                  @yield('panel-heading')
-                </div>
 
-                @yield('panel-body')
+  <div class="app_icon">
+    <a href="{{ url('help') }}">
+      <img src="app/img/apps/soporte_icon.png" alt="Soporte"/>
+      <span class="app_text">
+        Ayuda <br/><br/>
+      </span>
+    </a>
+  </div>
 
-                <div class="panel-footer">
-                  @yield('panel-footer')
-                </div><!--footer-->
-              </div><!--panel-default-->
-            </div><!--col-md-12-->
-          </div><!--row-->
-        @endif
 
-      </div><!--content-wrap-->
-    </div><!-- END PAGE CONTENT -->
-  </div><!-- END PAGE CONTAINER -->
 
-@include('Panel.logout')
+  <div class="app_icon">
+    <a href="https://forms.gle/TyFMaLfCkZPz2kYk8" target="_blank">
+      <img src="app/img/apps/dpt_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text">
+        Declaración Patrimonial
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a href="{{ url('cms') }}">
+      <img src="app/img/apps/web_icon.png" alt="Página Web"/>
+      <span class="app_text">
+        Página Web<br><br>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a href="{{ url('pnt') }}">
+      <img src="app/img/apps/pnt_icon.png" alt="Transparencia"/>
+      <span class="app_text">
+        Transparencia <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a href="{{ url('/sevac') }}">
+      <img src="app/img/apps/sevac_icon.png" alt="Sevac"/>
+      <span class="app_text">
+        Sevac <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a href="{{ url('mail') }}">
+      <img src="app/img/apps/mail_icon.png" alt="Correo" />
+      <span class="app_text">
+        Correos <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a href="{{ url('/') }}">
+      <img class="proximamente" src="app/img/apps/apy_icon.png" alt="Apoyos"/>
+      <span class="app_text proximamente">
+        Apoyos <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a href="{{ url('/') }}">
+      <img class="proximamente" src="app/img/apps/atc_icon.png" alt="Atención Ciudadana"/>
+      <span class="app_text proximamente">
+        Atención Ciudadana <br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img src="app/img/apps/vtc_icon.png" class="proximamente" alt="Viáticos"/>
+      <span class="app_text proximamente">
+        Viáticos<br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/usuarios_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Usuarios <br><br>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/odp_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Oficialía de Partes
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/rut_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Trámites en Línea
+      </span>
+    </a>
+  </div>
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/fiel_icon.png" alt="Firma Electrónica"/>
+      <span class="app_text proximamente">
+        Firma electrónica
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/noi_icon.png" alt="Nómina"/>
+      <span class="app_text proximamente">
+        Nómina <br><br>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Sistema Contable
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Predial <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Nomina <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Agenda Municipal
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Trámites en Línea
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Atención Ciudadana
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Contraloría <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Catastro <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Planeación <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Requisiciones<br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Facturación <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Obras <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Pagos en línea<br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Caja <br/><br/>
+      </span>
+    </a>
+  </div>
+
+
+
+  <div class="app_icon">
+    <a>
+      <img class="proximamente" src="app/img/apps/proximamente_icon.png" alt="Declaración Patrimonial"/>
+      <span class="app_text proximamente">
+        Agua <br/><br/>
+      </span>
+    </a>
+  </div>
+
+</div>
+@endsection
