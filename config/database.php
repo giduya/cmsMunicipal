d476cc885e5d2aac17e1eeabc0a4e6b339028c3a
giduya <alan@edulap.com>
2023-02-02 14:29:35 -0600

    51

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
       'mongodb' => [
         'driver' => 'mongodb',
         'dsn' => 'mongodb+srv://admin:cBRtY2gZz!9uUfu@cluster0.ojo6r.mongodb.net',
-        'database' => 'bd_cms',
+        'database' => 'bd_',
       ],
       'mysql' => [
           'driver' => 'mysql',
