fe4ca96d06b18d822b13ef5ba43e755e646991eb
giduya <alan@edulap.com>
2023-02-02 15:34:48 -0600

    57

--- resources/views/Plantilla/index.blade.php
+++ resources/views/Plantilla/index.blade.php
@@ -1,3 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>José Sixto Verduzco</title>
 hola
+</head>
 </html>
